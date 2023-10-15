a9b2d64bd1cd00bae612d7071ca0b611a037bcb6
Jordan Rob <JordanRob@Jordan-Robs-MacBook-Air.local>
2023-10-15 23:28:40 +0300

    Switched from custom login view to default views

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path("login/", views.user_login, name="login"),
+    # Custom views
+    #path("login/", views.user_login, name="login"),
+
+    #default views
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('', views.dashboard, name='dashboard'),
+
 ]
