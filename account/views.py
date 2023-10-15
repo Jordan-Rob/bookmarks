96abec28a1c7a61e6be8fc8f93b33e886a0914df
Jordan Rob <JordanRob@Jordan-Robs-MacBook-Air.local>
2023-10-15 23:03:19 +0300

    setup Login functionality

--- account/views.py
+++ account/views.py
@@ -1,3 +1,26 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm
+
 
 # Create your views here.
+def user_login(request):
+    if request.method == "POST":
+        form = LoginForm(data=request.POST)
+        if form.is_valid():
+            clean_form = form.cleaned_data
+            user = authenticate(username=clean_form["username"], password=clean_form["password"])
+
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse("Logged in")
+                else:
+                    return HttpResponse("Disabled Account")
+            else:
+                return HttpResponse("Invalid Login Details Supplied!")
+    else:
+        form = LoginForm()
+
+    return render(request, "account/login.html", {"form": form})
