81cb0218b4a1430ba47596f275d1322a3192511e
Jordan Rob <JordanRob@Jordan-Robs-MacBook-Air.local>
2023-10-18 00:40:45 +0300

    added password change views and templates

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from . import views
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeDoneView, PasswordChangeView
 
 urlpatterns = [
     # Custom views
@@ -9,9 +9,11 @@ urlpatterns = [
     #default views
     #path('login/', LoginView.as_view(), name='login'),
     #path('logout/', LogoutView.as_view(), name='logout'),
+    path('password-change/', PasswordChangeView.as_view(), name="password_change"),
+    path('password-change/done', PasswordChangeDoneView.as_view(), name="password_change_done"),
 
     path('', include('django.contrib.auth.urls')),
-    
+
     path('', views.dashboard, name='dashboard'),
     path('register/', views.register, name='register'),
 
