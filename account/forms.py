e597e83cf700b994312a43fb644933ac7ee1702e
Jordan Rob <jordanrob709@gmail.com>
2023-11-16 18:19:32 +0300

    added custom authentication backends

--- account/forms.py
+++ account/forms.py
@@ -9,16 +9,23 @@ class LoginForm(forms.Form):
 
 class UserRegistrationForm(forms.ModelForm):
     password = forms.CharField(label='Password', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput)
+    password_two = forms.CharField(label='Confirm Pa', widget=forms.PasswordInput)
     class Meta:
         model = User
         fields = ['username', 'first_name', 'email']
 
     def clean_password(self):
         clean_data = self.cleaned_data
-        if clean_data['password'] != clean_data['password2']:
+        
+        if clean_data.get('password') != clean_data.get('password_two'):
             raise forms.ValidationError("Passwords do not match")
         return clean_data['password']
+    
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        if User.objects.filter(email=data).exists():
+            raise forms.ValidationError('Email already in use.')
+        return data
     
 
 class UserEditForm(forms.ModelForm):
@@ -30,3 +37,11 @@ class ProfileEditForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields =[ 'date_of_birth', 'photo']
+
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        qs = User.objects.exclude(id=self.instance.id).filter(email=data)
+
+        if qs.exists():
+            raise forms.ValidationError('This email is already in use.')
+        return data
